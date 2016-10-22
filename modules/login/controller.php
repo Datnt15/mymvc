ca7c5812ca0c51a8a834606e4a4d6ddb01debf38
Datnt15 <tiendatbt19@gmail.com>
2016-10-22 15:42:42 +0700

    Move checking method to login and register controller

--- modules/login/controller.php
+++ modules/login/controller.php
@@ -69,5 +69,47 @@ class Login extends Controller
             }
     	}
 	}
+
+    
+
+    /**
+     * [check_username description]
+     * @param  [type] $username   [description]
+     * @param  string $field_name [This checked field name]
+     * @return [array]             [checking status and its message]
+     */
+    protected function check_username($username, $field_name = ''){
+        $res = array();
+        if ($username === '') {
+            $res['stt'] = 'failure';
+            $res['message'] = $field_name . ' can\'t be null';
+        }
+        elseif ( !preg_match("/^[A-Za-z_0-9-]{3,15}[^'\x22\s@!]+$/", $username) ){
+            $res['stt'] = 'failure';
+            $res['message'] = $field_name . " containts only number, uppercase, lowercase, underscore(_), hyphen(-), maxlength is 15 and minlength is 3.";
+        }
+        return $res;
+    }
+
+
+    /**
+     * [check_password checking password]
+     * @param  [type] $pass       [description]
+     * @param  string $field_name [This checked field name]
+     * @return [array]             [checking status and its message]
+     */
+    protected function check_password($pass, $field_name = ''){
+        $res = array();
+        if ($pass === '') {
+            $res['stt'] = 'failure';
+            $res['message'] = $field_name . ' can\'t be null';
+        }
+        elseif ( !preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,}[^'\x22\s@!]+$/", $pass) ){
+            $res['stt'] = 'failure';
+            $res['message'] = $field_name . " containts at least one number, one upercase, one lowercase, minlength is 4, can't containt some special characters like ( @, !, ', '' ) and whitespace";
+        }
+        return $res;
+    }
+
 	
 }
