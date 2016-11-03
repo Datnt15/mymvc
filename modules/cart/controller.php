554e093f7eae61b5b357bca080a0d627da62287e
Datnt15 <tiendatbt19@gmail.com>
2016-11-03 09:26:49 +0700

    addon add item function

--- modules/cart/controller.php
+++ modules/cart/controller.php
@@ -4,10 +4,14 @@
  */
 class Cart extends Controller
 {
-	private $model;
-	function __construct() {
+    private $model;
+    function __construct() {
 
-		parent::__construct(__CLASS__);
+        parent::__construct(__CLASS__);
+        
+    }
+
+    private function check_login(){
         if (!$this->user->is_logged_in()) {
             header("Location: " . BASE_URL . "login");
         }
@@ -20,6 +24,7 @@ class Cart extends Controller
     }
 
     private function load_header($title = 'Hồ sơ cá nhân'){
+        $this->check_login();
         $user = $this->user->get_current_user();
         $user['number_cart'] = $this->model->get_cart_of( $this->user->get_id() );
         $user['number_order'] = $this->model->get_order_of( $this->user->get_id() );
@@ -29,7 +34,7 @@ class Cart extends Controller
 
     public function profile (){
         $this->load_header();
-		$message = $this->session->get('message');
+        $message = $this->session->get('message');
         if (!empty($message)) { ?>
             <div class="alert alert-success" style="margin: 20px;">
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
@@ -38,9 +43,9 @@ class Cart extends Controller
         <?php
         $this->session->set('message', '');
         }
-		$this->load_view('edit-profile', $this->user->get_current_user());
+        $this->load_view('edit-profile', $this->user->get_current_user());
         $this->load_footer();
-	}
+    }
 
     public function add_product(){
         $this->load_header("Thêm sản phẩm mới");
@@ -49,7 +54,6 @@ class Cart extends Controller
     }
 
     public function index(){
-       
         if (!empty($message)) { ?>
             <div class="alert alert-success" style="margin: 20px;">
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
@@ -96,10 +100,77 @@ class Cart extends Controller
             unlink( $this->input->post('img') );
             $this->model->delete_cart( array('cid' => $this->input->post('cid') ) );
         }
-        $cart = $this->model->get_cart_of( $this->user->get_id() );
+        $uid = $this->user->get_id();
+        $addon_token = $this->cookie->get('addon_token');
+        if ($addon_token !== '' && $addon_token !== NULL) {
+            $this->model->update_cart( 
+                array( 'uid' => $uid ), 
+                array( 'uid' => $addon_token ) );
+        }
         $this->load_header("Giỏ hàng");
-        $this->load_view('cart', $cart);
+        $this->load_view('cart', $this->model->get_cart_of( $uid ) );
         $this->load_footer();
+    }
+
+
+    public function addon_add_item(){
+        // Allow from any origin
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+            // you want to allow, and if so:
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }
+
+        // Access-Control headers are received during OPTIONS requests
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+            exit(0);
+        }
+
+        if (isset($_POST['is_addon'])) {
+
+            $uid = $this->user->get_id();
+            if($uid == 0){
+                $addon_token = $this->cookie->get('addon_token');
+                if ($addon_token == '' || $addon_token == NULL) {
+                    $addon_token = md5( uniqid( time() ) );
+                    $this->cookie->set( 'addon_token', $addon_token );
+                }
+                $uid = $addon_token;
+            }
+            $item = array(
+                'url'           => $this->input->post('item_url'),
+                'name'          => $this->input->post('item_name'),
+                'img'           => $this->input->post('item_image'),
+                'uid'           => $uid,
+                'price'         => $this->input->post('item_price'),
+                'size'          => $this->input->post('item_color_size'),
+                'quantity'      => $this->input->post('quantity'),
+                'currency'      => $this->input->post('currency'),
+                'is_add_on'     => $this->input->post('is_addon'),
+                'type'          => $this->input->post('type'),
+                'seller_id'     => $this->input->post('seller_id'),
+                'seller_name'   => $this->input->post('seller_name'),
+                'note'          => $this->input->post('note'),
+                'shipcharge'    => $this->input->post('shipcharge')
+                );
+            // print_r($item); 
+            $cid = $this->model->add_to_cart($item);
+
+            if ( $cid ) {
+                echo 'Thêm sản phẩm thành công';
+            }else {
+                echo "Thêm sản phẩm thất bại";
+            }
+        }
     }
 
     public function update_cart_info(){
@@ -198,22 +269,24 @@ class Cart extends Controller
                 );
             }
         }
+
+
         $this->load_header("Quản lý đơn hàng");
-        $this->load_view('order',$this->model->get_order_of( $uid ));
+        $this->load_view('order', $this->model->get_order_of( $uid ) );
         $this->load_footer();
     }
 
 
-	public function update_user_info(){
+    public function update_user_info(){
         
-		if( isset($_POST) ){
-			
-			$data = array(
-				'phone' 		=> $this->input->post('phone'),
-				'address' 		=> $this->input->post('address'),
-				'birthday'    	=> $this->input->post('birthday'),
-            	);
-			
+        if( isset($_POST) ){
+            
+            $data = array(
+                'phone'         => $this->input->post('phone'),
+                'address'       => $this->input->post('address'),
+                'birthday'      => $this->input->post('birthday'),
+                );
+            
 
             $uid = $this->user->get_id();
                         
@@ -255,7 +328,7 @@ class Cart extends Controller
             $user = $this->user->update_user_info($data, $uid);
 
             if ( $user ) {
-            	$this->session->set('message', 'Cập nhật thành công');
+                $this->session->set('message', 'Cập nhật thành công');
                 ?>
                 <script>
                     window.location.replace($('base').attr('href') + 'cart/profile');
@@ -264,7 +337,7 @@ class Cart extends Controller
             }
             // Thất bại tràn trề
             else {
-            	$this->session->set('message', 'Đã có lỗi xảy ra!' );
+                $this->session->set('message', 'Đã có lỗi xảy ra!' );
                 ?>
                 <script>
                     window.location.replace($('base').attr('href') + 'cart/profile');
@@ -275,7 +348,7 @@ class Cart extends Controller
         }
         $this->load_header();
         $this->load_footer();
-	}
+    }
 
     private function load_footer(){
         $this->load_view('footer');
